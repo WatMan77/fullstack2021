1622f45640573088db7291411d122473075e5964
Daniel Kaluski <daniel.kaluski@aalto.fi>
2021-07-21 14:04:33 +0300

    7b done

--- part7OG/ultimate-hooks/src/App.js
+++ part7OG/ultimate-hooks/src/App.js
@@ -20,10 +20,14 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([])
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl).then(x => setResources(x.data))
+  }, [])
 
-  const create = (resource) => {
-    // ...
+  const create = async(resource) => {
+    const response = await axios.post(baseUrl, resource)
+    setResources(resources.concat(response.data))
+    return response.data
   }
 
   const service = {
